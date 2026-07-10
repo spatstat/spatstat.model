242094ae647b7e87f72c413b92937062da4e88ce
Adrian Baddeley <Adrian.Baddeley@curtin.edu.au>
2026-07-10 19:53:17 +0800

    Internal improvements to objsurf

--- R/objsurf.R
+++ R/objsurf.R
@@ -3,7 +3,7 @@
 #
 #  surface of the objective function for an M-estimator
 #
-#  $Revision: 1.34 $ $Date: 2025/12/07 02:35:48 $
+#  $Revision: 1.35 $ $Date: 2026/07/10 03:17:26 $
 #
 
 objsurf <- function(x, ...) {
@@ -22,7 +22,7 @@ objsurf.kppm <- objsurf.dppm <- function(x, ...,
     dotargs <- list(...)
     if(!is.null(parmap <- dotargs$parmap)) {
       ## transform to new parametrisation
-      tran <- parmap[[1]]
+      tran <- parmap[[1L]]
       tranpars <- as.data.frame(t(apply(h, 1, tran)))
       h <- cbind(tranpars, value=h$value)
     }
@@ -97,44 +97,70 @@ objsurfEngine <- function(objfun, optpar, objargs,
   if(npar != 2)
     stop("Only implemented for functions of 2 arguments")
   ## create grid of values of (possibly transformed) parameters  
+  if(!is.null(xlim)) check.range(xlim)
+  if(!is.null(ylim)) check.range(ylim)
   ratio <- ensure2vector(ratio)
   ngrid <- ensure2vector(ngrid)
   stopifnot(all(ratio > 1))
-  if(is.null(parmap)) {
-    ## use original parameters
-    if(is.null(xlim)) xlim <- optpar[1] * c(1/ratio[1], ratio[1])
-    if(is.null(ylim)) ylim <- optpar[2] * c(1/ratio[2], ratio[2])
-    xgrid <- seq(xlim[1], xlim[2], length=ngrid[1])
-    ygrid <- seq(ylim[1], ylim[2], length=ngrid[2])
+  if(untransformed <- is.null(parmap)) {
+    ## parameter values will be evenly spaced 
+    if(is.null(xlim)) xlim <- optpar[1L] * c(1/ratio[1L], ratio[1L])
+    if(is.null(ylim)) ylim <- optpar[2L] * c(1/ratio[2L], ratio[2L])
+    xgrid <- seq(xlim[1L], xlim[2L], length=ngrid[1L])
+    ygrid <- seq(ylim[1L], ylim[2L], length=ngrid[2L])
     pargrid <- expand.grid(xgrid, ygrid)
     colnames(pargrid) <- names(optpar)
+    Toptpar <- optpar
   } else {
-    if(!(length(parmap) == 2 && all(sapply(parmap, is.function))))
-      stop("parmap should be a list of 2 functions")
-    tran <- parmap[[1]]
-    invtran <- parmap[[2]]
-    ## transformed parameters
-    Toptpar <- tran(optpar) 
-    if(is.null(xlim)) xlim <- Toptpar[1] * c(1/ratio[1], ratio[1])
-    if(is.null(ylim)) ylim <- Toptpar[2] * c(1/ratio[2], ratio[2])
-    xgrid <- seq(xlim[1], xlim[2], length=ngrid[1])
-    ygrid <- seq(ylim[1], ylim[2], length=ngrid[2])
-    Tpargrid <- expand.grid(xgrid, ygrid)
-    colnames(Tpargrid) <- names(Toptpar)
-    ## inverse transform
+    ## parameter values will be evenly spaced on transformed scale
+    ## e.g. parmap=list(log=log, exp=exp)
+    if(!(is.list(parmap) &&
+         length(parmap) == 2 &&
+         sum(nzchar(names(parmap))) == 2 &&
+         all(sapply(parmap, is.function))))
+      stop("Argument parmap should be a named list of 2 functions", call.=FALSE)
+    tran <- parmap[[1L]]
+    invtran <- parmap[[2L]]
+    ## transformed parameter values
+    Toptpar <- tran(optpar)
+    if(length(Toptpar) != 2)
+      stop("parmap[[1]] is not vectorised", call.=FALSE)
+    if(is.null(xlim)) {
+      Txlim <- Toptpar[1L] * c(1/ratio[1L], ratio[1L])
+      xlim  <- invtran(Txlim)
+    } else {
+      Txlim <- tran(xlim)
+    }
+    if(is.null(ylim)) {
+      Tylim <- Toptpar[2L] * c(1/ratio[2L], ratio[2L])
+      ylim  <- invtran(Tylim)
+    } else {
+      Tylim <- tran(ylim)
+    }
+    Txgrid <- seq(Txlim[1L], Txlim[2L], length=ngrid[1L])
+    Tygrid <- seq(Tylim[1L], Tylim[2L], length=ngrid[2L])
+    Tpargrid <- expand.grid(Txgrid, Tygrid)
+    colnames(Tpargrid) <- names(optpar)
+    ## parameter values
     pargrid <- t(apply(Tpargrid, 1, invtran))
     colnames(pargrid) <- names(optpar)
-    ## finally overwrite optimal values
-    optpar <- Toptpar
   }
   # evaluate objective function
   if(verbose) cat(paste("Evaluating", nrow(pargrid), "function values..."))
   values <- do.call(apply,
                     append(list(pargrid, 1, objfun, objargs=objargs), dotargs))
   if(verbose) cat("Done.\n")
-  result <- list(x=xgrid, y=ygrid, z=matrix(values, ngrid[1], ngrid[2]))
+  result <- list(
+    x = if(untransformed) xgrid else Txgrid,
+    y = if(untransformed) ygrid else Tygrid,
+    z = matrix(values, ngrid[1], ngrid[2])
+  )
   attr(result, "optpar") <- optpar
+  attr(result, "optpar.tran") <- Toptpar
   attr(result, "objname") <- objname
+  attr(result, "xlim") <- xlim
+  attr(result, "ylim") <- ylim
+  attr(result, "parmap") <- parmap # could be NULL
   class(result) <- "objsurf"
   return(result)
 }
@@ -143,23 +169,26 @@ print.objsurf <- function(x, ...) {
   cat("Objective function surface\n")
   optpar <- attr(x, "optpar")
   objname <- attr(x, "objname")
+  xlim <- attr(x, "xlim") %orifnull% range(x$x)
+  ylim <- attr(x, "ylim") %orifnull% range(x$y)
   nama <- names(optpar)
   cat(paste("\tFunction value:", objname, "\n"))
   cat("Parameter limits:\n")
-  cat(paste("\t", paste0(nama[1L], ":"), prange(signif(range(x$x), 4)), "\n"))
-  cat(paste("\t", paste0(nama[2L], ":"), prange(signif(range(x$y), 4)), "\n"))
+  cat(paste("\t", paste0(nama[1L], ":"), prange(signif(xlim, 4)), "\n"))
+  cat(paste("\t", paste0(nama[2L], ":"), prange(signif(ylim, 4)), "\n"))
   if(!is.null(attr(x, "h")))
     splat("[Includes history of evaluations of objective function]")
   invisible(NULL)
 }
 
 summary.objsurf <- function(object, ...) {
-  y <- list(xrange=range(object$x),
-            yrange=range(object$y),
-            objrange=range(object$z),
-            optpar=as.list(attr(object, "optpar")),
-            objname=attr(object, "objname")
-            , trace=attr(object, "h") # may be NULL
+  y <- list(xrange   = attr(object, "xlim") %orifnull% range(object$x),
+            yrange   = attr(object, "ylim") %orifnull% range(object$y),
+            objrange = range(object$z),
+            optpar   = as.list(attr(object, "optpar")),
+            objname  = attr(object, "objname"),
+            parmap   = attr(object, "parmap"), # could be NULL
+            trace    = attr(object, "h") # may be NULL
             )
   class(y) <- c("summary.objsurf", class(y))
   return(y)
@@ -193,15 +222,16 @@ image.objsurf <- plot.objsurf <- function(x, ...) {
           resolve.defaults(list(x=quote(xx)), 
                            list(...),
                            list(xlab=nama[1L], ylab=nama[2L], main=xname)))
-  abline(v=optpar[1L], lty=3)
-  abline(h=optpar[2L], lty=3)
+  ## plot coordinates of optimal parameter values in transformed scale
+  optpar.tran <- attr(x, "optpar.tran") %orifnull% optpar
+  abline(v=optpar.tran[1L], lty=3)
+  abline(h=optpar.tran[2L], lty=3)
   return(invisible(NULL))
 }
 
 lines.objsurf <- function(x, ..., directed=FALSE) {
-  sx <- summary(x)
-  if(!is.null(h <- sx[["trace"]])) {
-    lines(h, ..., directed=directed)
+  if(!is.null(h <- attr(x, "trace"))) {
+    lines(h, ..., directed=directed) # trace is already in transformed coords
   } else message("No trajectory data")
   return(invisible(NULL))
 }
@@ -209,6 +239,7 @@ lines.objsurf <- function(x, ..., directed=FALSE) {
 contour.objsurf <- function(x, ...) {
   xname <- short.deparse(substitute(x))
   optpar <- summary(x)[["optpar"]]
+  tran <- attr(x, "parmap")[[1L]] %orifnull% function(x) { x }  
   nama <- names(optpar)
   xx <- unclass(x)
   dont.complain.about(xx)
@@ -216,8 +247,8 @@ contour.objsurf <- function(x, ...) {
           resolve.defaults(list(x=quote(xx)), 
                            list(...),
                            list(xlab=nama[1], ylab=nama[2], main=xname)))
-  abline(v=optpar[1], lty=3)
-  abline(h=optpar[2], lty=3)
+  abline(v=tran(optpar[1]), lty=3)
+  abline(h=tran(optpar[2]), lty=3)
   return(invisible(NULL))
 }
 
